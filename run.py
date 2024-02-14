985f80e6deb4c582d08f00c9cd767d1aed789613
SiriusAhu <SiriusLLLUUU@outlook.com>
2024-02-14 23:38:10 +0800

    Updated verbose output: Box pos, prob, class

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ WINDOW_NAME = "YOLO REAL TIME OBJECT DETECTION!!!"
 
 args = argparse.ArgumentParser()
 args.add_argument("-m", "--model", type=str, default="8n", help="Model to use: Enter \"8n\" for YOLOv8n, \"5s\" for YOLOv5s")
-args.add_argument("-v", "--verbose", type=bool, default=False, help="\"True\" or \"False\" | Set to True for verbose output for \"model.predict()\"")
+args.add_argument("-v", "--verbose", action="store_true", default=False, help="\"True\" or \"False\" | Set to True for verbose output for \"model.predict()\"")
 args.add_argument("-d", "--device", type=int, default=1, help="\"0\" or 1 | Device to use: 0 for CPU, 1 for GPU | If you have more than one GPU, you must be able to modify this code by yourself ^^")
 args.add_argument("-c", "--camera", type=int, default=0, help="Camera to use: 0 for the first camera | Index is based on your system's camera index | Try to change this value if you meet errors")
 args.add_argument("-mf", "--model_folder", type=str, default="models", help="Folder to save the model (under current directory) | Default: \"models\" (created if not exist)")
@@ -46,6 +46,20 @@ model = YOLO(os.path.join(model_folder, model_name))
 # Open the camera
 cap = cv2.VideoCapture(camera)
 
+def show_result_info(result):
+    boxes = result.boxes.xywh.cpu()
+    origin_shape = result.boxes.orig_shape
+    clss = result.boxes.cls.int().cpu().tolist()
+    names = result.names
+    confs = result.boxes.conf.float().cpu().tolist()
+
+    print(f"- Origin Shape: {origin_shape}")
+    for box, cls, conf in zip(boxes, clss, confs):
+        x, y, w, h = box
+        label = f"{names[cls]}: {conf:.2f}"
+        x1, y1, x2, y2 = int(x - w / 2), int(y - h / 2), int(x + w / 2), int(y + h / 2)
+        print(f"- {label} at ({x1}, {y1}) to ({x2}, {y2})")
+
 while cap.isOpened():
     # Read the frame
     ret, frame = cap.read()
@@ -53,6 +67,10 @@ while cap.isOpened():
     # Predict the frame
     results = model.predict(frame, verbose=verbose, device=device)
     img = np.squeeze(results[0].plot()) # Convert to numpy array
+
+    # Verbose with the result
+    if verbose:
+        show_result_info(results[0])
 
     # Show the frame in opencv window
     cv2.imshow(WINDOW_NAME, img)
